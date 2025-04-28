90a008e5344c2ee125b0acb31a2595883dbcde8f
Li-Wen Hsu <lwhsu@lwhsu.org>
2025-04-28 12:48:42 +0800

    net-im/element-desktop: Update to 1.11.99

--- net-im/element-desktop/files/patch-electron-builder.ts
+++ net-im/element-desktop/files/patch-electron-builder.ts
@@ -1,18 +1,12 @@
---- electron-builder.ts.orig	2024-12-17 13:50:37 UTC
-+++ electron-builder.ts
-@@ -51,7 +51,7 @@ const config: Writable<Configuration> = {
- const config: Writable<Configuration> = {
-     appId: "im.riot.app",
-     asarUnpack: "**/*.node",
+--- electron-builder.ts.orig	2025-04-23 19:15:41.000000000 +0800
++++ electron-builder.ts	2025-04-28 15:02:02.357570000 +0800
+@@ -90,9 +90,9 @@
+         loadBrowserProcessSpecificV8Snapshot: false,
+         enableEmbeddedAsarIntegrityValidation: true,
+     },
 -    afterPack: async (context: AfterPackContext) => {
 +    /* afterPack: async (context: AfterPackContext) => {
-         if (context.electronPlatformName !== "darwin" || context.arch === Arch.universal) {
-             // Burn in electron fuses for proactive security hardening.
-             // On macOS, we only do this for the universal package, as the constituent arm64 and amd64 packages are embedded within.
-@@ -87,7 +87,7 @@ const config: Writable<Configuration> = {
-                 [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: false,
-             });
-         }
+         await injectAsarIntegrity(context);
 -    },
 +    }, */
      files: [
